d4ab542a38270a1b86e22c1b9355a99c36afb0bb
jclay99 <46943006+jclay99@users.noreply.github.com>
2021-05-05 16:08:44 -0700

    Add files via upload

---
+++ GenbankAsAFunction/src/com/example/GenbankAsAFunction/GenbankToPolynucleotide.java
@@ -0,0 +1,25 @@
+package com.example.GenbankAsAFunction;
+
+/** This class reads Genbank files and converts them to Polynucleotide objects,
+ * or models describing linear, circular, double stranded, and single stranded
+ * DNA sequences. The Model class is imported from the biojava library, an open
+ * source library for DNA sequencing. */
+
+public class GenbankToPolynucleotide {
+
+    /** Instantiates the genbankFile variable; this variable is assigned in main. */
+    private static String genbankFile;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void readFile(String filePath) {
+
+    }
+
+    public static void convert() {
+
+    }
+
+}
---
+++ GenbankAsAFunction/src/com/example/GenbankAsAFunction/PolynucleotideToGenbank.java
@@ -0,0 +1,25 @@
+package com.example.GenbankAsAFunction;
+
+/** This class reads Polynucleotide objects, or models describing linear,
+ * circular, double stranded, and single stranded DNA sequences, and converts
+ * them to annotated Genbank files. The Model class is imported from the
+ * biojava library, an open source library for DNA sequencing. */
+
+public class PolynucleotideToGenbank {
+
+    /** Instantiates the PolynucleotideFile variable; this variable is assigned in main. */
+    private static String polynucleotideFile;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void readFile(String filePath) {
+
+    }
+
+    public static void convert() {
+
+    }
+
+}
